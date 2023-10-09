b3cfcfb28edfba3f54836f4b4545255f45591447
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-10-09 12:39:45 +0530

    Add undo and redo ability for canvas

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -6,6 +6,8 @@ const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
+  const drwaHistory = useRef([]);
+  const historyPointer = useRef(0);
   const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
@@ -23,9 +25,19 @@ const Board = () => {
       anchor.href = url;
       anchor.download = "sketch.jpg";
       anchor.click();
+    }else if(actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO){
+      if(historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO){
+       historyPointer.current -= 1;
+      }
+
+      if(historyPointer.current < drwaHistory.current.length-1 && actionMenuItem === MENU_ITEMS.REDO){
+        historyPointer.current += 1;
+      }
+      const imageData = drwaHistory.current[historyPointer.current];
+      context.putImageData(imageData,0,0);
     }
     dispatch(actionItemClick(null));
-  }, [actionMenuItem]);
+  }, [actionMenuItem,dispatch]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -67,6 +79,9 @@ const Board = () => {
     };
     const handleMouseUp = (e) => {
       shouldDraw.current = false;
+      const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+      drwaHistory.current.push(imageData);
+      historyPointer.current = drwaHistory.current.length-1 ;
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
